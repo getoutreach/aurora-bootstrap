bf657b81486bd3f6629c3451f1cb1d20fa7ad24d
Greg Orlov <gaorlov@gmail.com>
2019-08-27 14:43:00 -0700

    fixing zeitwork loading

--- lib/aurora_bootstrapper.rb
+++ lib/aurora_bootstrapper.rb
@@ -1,14 +1,6 @@
 require "json"
 
 module AuroraBootstrapper
-  autoload :Converter, "converter"
-  autoload :CsvParser, "csv_parser"
-  autoload :Database,  "database"
-  autoload :Exporter,  "exporter"
-  autoload :File,      "file"
-  autoload :Logger,    "logger"
-  autoload :Table,     "table"
-
   ROW_DELIMITER = -'AURORA-BOOTSTRAP-EXPORT-ROW-DELIMITER'
   COL_DELIMITER = -'AURORA-BOOTSTRAP-EXPORT-COL-DELIMITER'
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
-require 'zeitwerk'
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "zeitwerk"
 require 'simplecov'
 require 'dotenv'
 require 'mysql2'
@@ -100,6 +100,11 @@ module Gls
   end
 end
 
-require_relative "../lib/aurora_bootstrapper"
+loader = Zeitwerk::Loader.new
+loader.log!
+loader.push_dir( File.expand_path( "../../lib/", __FILE__) )
+loader.setup
+loader.eager_load
+
 require "minitest/autorun"
 require "mocha/minitest"
