a5207f170f2b181f927c26bf7be2a61427ea1378
Greg Orlov <gaorlov@gmail.com>
2021-02-08 18:21:05 -0800

    adding support for table whitelisting

--- lib/aurora_bootstrapper/database.rb
+++ lib/aurora_bootstrapper/database.rb
@@ -1,17 +1,21 @@
 module AuroraBootstrapper
   class Database
-    def initialize( database_name:, client:, blacklisted_tables: [], blacklisted_fields: [] )
+    def initialize( database_name:, client:, blacklisted_tables: [], whitelisted_tables: [], blacklisted_fields: [] )
       @database_name      = database_name
       @blacklisted_tables = blacklisted_tables
+      @whitelisted_tables = whitelisted_tables
       @blacklisted_fields = blacklisted_fields
       @client             = client
     end
 
     def table_names
-      @table_names ||= @client.query( "SHOW TABLES IN `#{@database_name}`" ).map do | row |
-        row[ "Tables_in_#{@database_name}" ]
-      end.reject do | table_name |
-        blacklisted_table?( table_name )
+      @table_names ||= begin
+        # if we only whitelist, assume that everything else is blacklisted
+        filtered_tables = whitelist_only? ? [] : tables
+
+        filtered_tables -= blacklisted unless blacklisted.empty?
+        filtered_tables += whitelisted unless whitelisted.empty?
+        filtered_tables
       end
     end
 
@@ -26,21 +30,45 @@ module AuroraBootstrapper
       end
     end
 
-    def blacklisted_table?( table_name )
-      @blacklisted_tables.any? do | blacklisted_table |
+    private
+
+    def tables
+      @tables ||= @client.query( "SHOW TABLES IN `#{@database_name}`" ).map do | row |
+        row[ "Tables_in_#{@database_name}" ]
+      end
+    end
+
+    def blacklisted
+      tables.select do | table_name |
+        matches? table_name, @blacklisted_tables
+      end
+    end
+
+    def whitelisted
+      tables.select do | table_name |
+        matches? table_name, @whitelisted_tables
+      end
+    end
+
+    def whitelist_only?
+      @blacklisted_tables.empty? and !@whitelisted_tables.empty?
+    end
+
+    def matches?( table_name, filter_list )
+      filter_list.any? do | filtered_table |
         # blacklisted tables can be in the format of "table" or "database.table"
         
-        bl_table_name    = blacklisted_table
+        bl_table_name    = filtered_table
         bl_database_name = @database_name
 
-        if blacklisted_table.match( /\/.*\// )
-          regexp         = blacklisted_table.slice(1...-1)
+        if filtered_table.match( /\/.*\// )
+          regexp         = filtered_table.slice(1...-1)
           qualified_name = "#{@database_name}.#{table_name}"
 
           bl_table_name  = qualified_name.match( /#{regexp}/ ) ? table_name : false
         
-        elsif blacklisted_table.match( /[a-zA-Z0-9_]+\.[a-zA-Z0-9_]+/ )
-          bl_database_name, bl_table_name = blacklisted_table.split(".")
+        elsif filtered_table.match( /[a-zA-Z0-9_]+\.[a-zA-Z0-9_]+/ )
+          bl_database_name, bl_table_name = filtered_table.split(".")
         end
 
         bl_table_name    == table_name &&
